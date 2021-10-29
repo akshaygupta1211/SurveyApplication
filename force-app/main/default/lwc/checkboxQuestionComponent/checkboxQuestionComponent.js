0354045cf0ee2abe63687d6bc5736c573aa6ab5b
Akshay Gupta <akshaygupta1211@users.noreply.github.com>
2021-10-30 00:02:54 +0530

    checkbox render issue fixed

--- force-app/main/default/lwc/checkboxQuestionComponent/checkboxQuestionComponent.js
+++ force-app/main/default/lwc/checkboxQuestionComponent/checkboxQuestionComponent.js
@@ -2,6 +2,30 @@ import { LightningElement, api } from 'lwc';
 
 export default class CheckboxQuestionComponent extends LightningElement {
     @api question;
-    value;
-    @api key;    
+    @api value;
+    @api key;
+    options = [];
+    selectedOption;
+
+    connectedCallback() {
+        for(let i = 0; i < this.question.answers.length; i++) {
+            let nameOriginal = JSON.stringify(this.question.answers[i].Name);
+            let nameModified = nameOriginal.replaceAll('"', '');
+            this.options.push( {label: nameModified, value: nameModified });
+        }
+    }
+    
+    handleChange(event) {
+        this.selectedOption = event.detail.value;
+        const selectedEvent = new CustomEvent('selected', 
+        { 
+            detail : {
+                response: this.selectedOption, 
+                questionId : this.question.questionId
+            } 
+        });
+
+        this.dispatchEvent(selectedEvent);        
+    }
+
 }
--- force-app/main/default/lwc/radionButtonQuestionComponent/radionButtonQuestionComponent.js
+++ force-app/main/default/lwc/radionButtonQuestionComponent/radionButtonQuestionComponent.js
@@ -2,9 +2,8 @@ import { LightningElement, api } from 'lwc';
 
 export default class RadionButtonQuestionComponent extends LightningElement {
     @api question;
-    value;
+    @api value;
     @api key;
-    options = [];
     selectedOption;
 
     connectedCallback() {
