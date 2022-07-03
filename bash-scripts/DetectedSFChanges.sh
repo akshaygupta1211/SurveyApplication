34d06aeb9edf2b695083ec1776f84151ed80a65f
Akshay Gupta <26359906+akshaygupta-dev@users.noreply.github.com>
2022-07-03 16:56:30 +0530

    fixing error

--- bash-scripts/DetectedSFChanges.sh
+++ bash-scripts/DetectedSFChanges.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #Check if changed-sources contain salesforce changes
-if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default ]]
+ls -la
+if [[ -d changed-sources/force-app/main/default ]]
 then
     echo "Changes detected in following file(s):"
     #Loop through all the test classes files and store the names in csv format    
-    for file in ${REPO_NAME}/changed-sources/force-app/main/default/*/*
+    for file in changed-sources/force-app/main/default/*/*
     do
         echo "${file##*/}"
     done
