2c7279f374204425a982ea96670552fde298ac13
Akshay Gupta <26359906+akshaygupta-dev@users.noreply.github.com>
2022-07-03 13:24:13 +0530

    fixing error and added logic to print changed file(s) name

---
+++ bash-scripts/DetectedSFChanges.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Check if changed-sources contain salesforce changes
+if [[ -d changed-sources/force-app/main/default ]]
+then
+    echo "Changes detected in following file(s):"
+    #Loop through all the test classes files and store the names in csv format    
+    for file in changed-sources/force-app/main/default/*/*
+    do
+        echo {"$file"}
+    done
+fi
