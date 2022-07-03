770f6271b4bf8801dc037ceaeb6b64c541e31d63
Akshay Gupta <26359906+akshaygupta-dev@users.noreply.github.com>
2022-07-03 17:28:15 +0530

    moved code scan logic to bash

---
+++ bash-scripts/RunCodeScan.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#If changes detected are salesforce related code changes
+if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default ]]
+then 
+    sfdx scanner:run --target "${{ github.event.repository.name }}/changed-sources/force-app/main/default" --engine "eslint-lwc,retire-js,cpd,pmd" --verbose
+#If changes detected are not salesforce related code changes
+else
+then 
+    echo "No code change detected to run scan"
+fi
