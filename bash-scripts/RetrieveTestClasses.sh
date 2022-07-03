36c2936776e6631c151dcddb9e80e11220acd3bc
Akshay Gupta <26359906+akshaygupta-dev@users.noreply.github.com>
2022-07-03 16:28:15 +0530

    optimizing jobs

--- bash-scripts/RetrieveTestClasses.sh
+++ bash-scripts/RetrieveTestClasses.sh
@@ -11,14 +11,20 @@ then
     done
     FILE_NAMES=${FILE_NAMES:0:${#FILE_NAMES}-1}
 fi
+
 echo "Changes detected in following test class(es): ${FILE_NAMES}"
-#If no changes detected in test class(es) just run validation
-if [ ${#FILE_NAMES} == 0 ]
+
+#Check if changed-sources contain salesforce changes
+if [[ -d changed-sources/force-app/main/default ]]
 then
-    echo "Running validation"
-    sfdx force:source:deploy -c -x ${REPO_NAME}/changed-sources/package/package.xml --verbose
-#If changes detected in test class(es) run test class(es) and validation  
-else
-    echo "Running validation with test class(es)"
-    sfdx force:source:deploy -c -x ${REPO_NAME}/changed-sources/package/package.xml --testlevel RunSpecifiedTests --runtests "${FILE_NAMES}" --verbose
-fi    
+    #If no changes detected in test class(es) just run validation
+    if [ ${#FILE_NAMES} == 0 ]
+    then
+        echo "Running validation"
+        sfdx force:source:deploy -c -x ${REPO_NAME}/changed-sources/package/package.xml --verbose
+    #If changes detected in test class(es) run test class(es) and validation  
+    else
+        echo "Running validation with test class(es)"
+        sfdx force:source:deploy -c -x ${REPO_NAME}/changed-sources/package/package.xml --testlevel RunSpecifiedTests --runtests "${FILE_NAMES}" --verbose
+    fi
+fi        
