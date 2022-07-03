eda522dc80630e8f4d7778694ad930e7246b7c5c
Akshay Gupta <26359906+akshaygupta-dev@users.noreply.github.com>
2022-07-03 17:35:08 +0530

    fixed syntax error

--- bash-scripts/RunCodeScan.sh
+++ bash-scripts/RunCodeScan.sh
@@ -3,7 +3,7 @@
 #If changes detected are salesforce related code changes
 if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default ]]
 then 
-    sfdx scanner:run --target "${{ github.event.repository.name }}/changed-sources/force-app/main/default" --engine "eslint-lwc,retire-js,cpd,pmd" --verbose
+    sfdx scanner:run --target "${REPO_NAME}/changed-sources/force-app/main/default" --engine "eslint-lwc,retire-js,cpd,pmd" --verbose
 #If changes detected are not salesforce related code changes
 else
     echo "No code change detected to run scan"
