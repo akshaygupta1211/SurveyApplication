8d4c07ccd904aac3ed358c095d2c1b6fc3986184
Akshay Gupta <26359906+akshaygupta-dev@users.noreply.github.com>
2022-07-03 11:57:40 +0530

    added script to run test class with validation if changes detected in test class

--- bash-scripts/RetrieveTestClasses.sh
+++ bash-scripts/RetrieveTestClasses.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+#Check if changed-sources contain classes
 if [[ -d changed-sources/force-app/main/default/classes ]]
 then
+    #Change the current directory to classes
     cd changed-sources/force-app/main/default/classes
-    FILES=$(ls *.cls)
+    
+    FILES=$(ls *Test.cls)
     FILE_NAMES=""
+    
+    #Loop through all the files and store the names in csv format    
     for file in $FILES
     do
         FILE_NAMES+="$file,"
     done
-    echo $FILE_NAMES
+
 fi
+
+if [[ ${#FILE_NAMES} == 0]]
+then
+    sfdx force:source:deploy -c -x ${{ github.event.repository.name }}/changed-sources/package/package.xml
+else
+    sfdx force:source:deploy -c -x ${{ github.event.repository.name }}/changed-sources/package/package.xml --classnames "${FILE_NAMES}" --resultformat tap --codecoverage    
