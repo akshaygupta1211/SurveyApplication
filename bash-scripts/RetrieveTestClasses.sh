a42c656691c1ad548aa3a7cfdbc1b4a1d7475ee7
Akshay Gupta <26359906+akshaygupta-dev@users.noreply.github.com>
2022-07-03 16:21:24 +0530

    added logic to print changed test class(es) name

--- bash-scripts/RetrieveTestClasses.sh
+++ bash-scripts/RetrieveTestClasses.sh
@@ -11,7 +11,7 @@ then
     done
     FILE_NAMES=${FILE_NAMES:0:${#FILE_NAMES}-1}
 fi
-
+echo "Changes detected in following test class(es): ${FILE_NAMES}"
 #If no changes detected in test class(es) just run validation
 if [ ${#FILE_NAMES} == 0 ]
 then
