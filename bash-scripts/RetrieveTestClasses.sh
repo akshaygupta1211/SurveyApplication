c512a49e7666c653b06aeedba74a50aa8df38c5d
d981560 <akshay.gupta@team.telstra.com>
2022-07-03 19:09:51 +0530

    fixed test class detection logic

--- bash-scripts/RetrieveTestClasses.sh
+++ bash-scripts/RetrieveTestClasses.sh
@@ -3,11 +3,11 @@
 #Check if changed-sources contain salesforce changes
 if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default ]]
 then
-    FILE_NAMES="" 
+    FILE_NAMES=""
     #Check if changed-sources contain classes
     if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default/classes ]]
     then
-        #Loop through all the test classes files and store the names in csv format   
+        #Loop through all the test classes files and store the names in csv format 
         for file in ${REPO_NAME}/changed-sources/force-app/main/default/classes/*Test.cls
         do
             FILE_NAMES+="$(basename ${file} .cls),"
@@ -18,7 +18,7 @@ then
     echo "Changes detected in following test class(es): ${FILE_NAMES}"
     
     #If no changes detected in test class(es) just run validation
-    if [ ${#FILE_NAMES} == 0 ]
+    if [ "${FILE_NAMES}" == "*Test," ]
     then
         echo "Running validation"
         sfdx force:source:deploy -c -x ${REPO_NAME}/changed-sources/package/package.xml --verbose
@@ -27,4 +27,5 @@ then
         echo "Running validation with test class(es)"
         sfdx force:source:deploy -c -x ${REPO_NAME}/changed-sources/package/package.xml --testlevel RunSpecifiedTests --runtests "${FILE_NAMES}" --verbose
     fi
+    unset FILE_NAMES
 fi        
