f0cfc27babe1078f618809651b920eee4cb69bea
d981560 <akshay.gupta@team.telstra.com>
2022-07-03 16:47:02 +0530

    fixing logical errors

--- bash-scripts/DetectedSFChanges.sh
+++ bash-scripts/DetectedSFChanges.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Check if changed-sources contain salesforce changes
-if [[ -d changed-sources/force-app/main/default ]]
+if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default ]]
 then
     echo "Changes detected in following file(s):"
     #Loop through all the test classes files and store the names in csv format    
-    for file in changed-sources/force-app/main/default/*/*
+    for file in ${REPO_NAME}/changed-sources/force-app/main/default/*/*
     do
-        echo {"$file"}
+        echo "$(basename ${file} .cls)"
     done
 fi
