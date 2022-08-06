b3c3766b1854b4539417fcc6d3ce6be64b15d299
Akshay Gupta <26359906+akshaygupta-dev@users.noreply.github.com>
2022-08-06 22:13:20 +0530

    added logic to read test classes from PR and detect changes in apex class

--- bash-scripts/RunValidationAndTestClasses.sh
+++ bash-scripts/RunValidationAndTestClasses.sh
@@ -7,8 +7,8 @@ then
     #Check if changed-sources contain classes
     if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default/classes ]]
     then
-        #Loop through all the test classes files and store the names in csv format 
-        for file in ${REPO_NAME}/changed-sources/force-app/main/default/classes/*Test.cls
+        #Loop through all the classes files and store the names in csv format 
+        for file in ${REPO_NAME}/changed-sources/force-app/main/default/classes/*.cls
         do
             FILE_NAMES+="$(basename ${file} .cls),"
         done
@@ -17,20 +17,20 @@ then
 
     if [ ${#FILE_NAMES} != 0 ]
     then
-        echo "Changes detected in following test class(es): ${FILE_NAMES}"
+        echo "Changes detected in following class(es): ${FILE_NAMES}"
     else
-        echo "No test class changes detected!"
+        echo "No class changes detected!"
     fi    
     
-    #If no changes detected in test class(es) just run validation
-    if [ "${FILE_NAMES}" == "*Test" ] || [ "${FILE_NAMES}" == "" ] 
+    #If no changes detected in class(es) just run validation
+    if [ "${FILE_NAMES}" == "" ] 
     then
         echo "Running validation"
         sfdx force:source:deploy -c -x ${REPO_NAME}/changed-sources/package/package.xml --verbose
-    #If changes detected in test class(es) run test class(es) and validation  
+    #If changes detected in class(es) run specified test class(es) and validation  
     else
         echo "Running validation with test class(es)"
-        sfdx force:source:deploy -c -x ${REPO_NAME}/changed-sources/package/package.xml --testlevel RunSpecifiedTests --runtests "${FILE_NAMES}" --verbose
+        sfdx force:source:deploy -c -x ${REPO_NAME}/changed-sources/package/package.xml --testlevel RunSpecifiedTests --runtests "${PR_BODY}" --verbose
     fi
     unset FILE_NAMES
 fi        
