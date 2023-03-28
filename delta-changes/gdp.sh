e45fa5bc6b0d22b85ed5184e6e1fc41410f5b9ba
Akshay Gupta <26359906+akshaygupta-dev@users.noreply.github.com>
2023-03-28 16:25:43 +0530

    added logic to update test class name in build file

--- delta-changes/gdp.sh
+++ delta-changes/gdp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # This is a Bash script that generates a package.xml file based on the changes made to files in a Salesforce development environment. 
-# The script uses Git to obtain a list of changed files between two brnaches and copies the changed files to a working directory. 
+# The script uses Git to obtain a list of changed files between two branches and copies the changed files to a working directory. 
 # It then reads through the list of changed files and generates a package.xml file based on the metadata type of the files.
 # The script uses two JSON files to map Salesforce directory names to their corresponding metadata types and to determine 
 # whether a metadata file exists for a given directory. It also handles exceptions for certain metadata types that have non-standard naming conventions.
@@ -17,14 +17,17 @@ declare -r OUTPUT_XML_FILE="$WORKING_DIR/package.xml"
 declare -r CHANGED_SRC_LIST_FILE="$SCRIPT_PATH/ChangedFileNames.txt"
 declare -r DIR_XML_NAME_JSON_DATA="$SCRIPT_PATH/DirectoryXMLName_v56.json"
 declare -r DIR_NAME_META_FILE_JSON_DATA="$SCRIPT_PATH/DirectoryNameMetafile_v56.json"
+declare -r TEST_CLASS_MAPPING_JSON_DATA="$SCRIPT_PATH/TestClassMapping.json"
 declare -r API_VERSION=51.0
 
 declare -a lines
+declare -a apex_classes_list=()
 declare -a exceptional_metadata=("customMetadata" "quickActions")
 
 declare -A exceptional_metadata_suffix=(
   ["customMetadata"]=".md"
   ["quickActions"]=".quickAction"
+  ["approvalProcesses"]=".approvalProcess"
 )
 
 # Parse the JSON text using jq and store the result in an associative array
@@ -36,25 +39,32 @@ declare -A dir_meta_file_exist_array
 
 eval $(jq -r 'to_entries[] | @sh "dir_meta_file_exist_array[\(.key|tostring)]=\(.value)"' < $DIR_NAME_META_FILE_JSON_DATA)
 
+declare -A test_class_mapping_array
+
+eval $(jq -r 'to_entries[] | @sh "test_class_mapping_array[\(.key|tostring)]=\(.value)"' < $TEST_CLASS_MAPPING_JSON_DATA)
+
 
 # Creating directory to store changed sources and package.xml
-if [[ -d "$WORKING_DIR" ]]; then
+if [[ -d "$WORKING_DIR" ]]
+then
   rm -Rf $WORKING_DIR || { echo "Error removing directory $WORKING_DIR"; exit 1; }
 fi
 
 mkdir -p $WORKING_DIR || { echo "Error creating directory $WORKING_DIR"; exit 1; }
 
-if [[ -f "$CHANGED_SRC_LIST_FILE" ]]; then
+if [[ -f "$CHANGED_SRC_LIST_FILE" ]]
+then
   rm $CHANGED_SRC_LIST_FILE || { echo "Error removing file $CHANGED_SRC_LIST_FILE"; exit 1; }
 fi
 
 touch $OUTPUT_XML_FILE || { echo "Error creating file $OUTPUT_XML_FILE"; exit 1; }
 touch $CHANGED_SRC_LIST_FILE || { echo "Error creating file $CHANGED_SRC_LIST_FILE"; exit 1; }
 
-pushd "$3" && git diff "$1..$2" --name-only --diff-filter=ACMRTUXB > "${CHANGED_SRC_LIST_FILE}" && popd || { echo "Error getting changed files list"; exit 1; }
+pushd "$3" && git checkout $1 && git pull && git checkout $2 && git pull && git diff "$1..$2" --name-only --diff-filter=ACMR > "${CHANGED_SRC_LIST_FILE}" && popd || { echo "Error getting changed files list"; exit 1; }
 
 while read line; do
-  if [[ "$line" == *"src/"* ]] && [[ "$line" != *"src/package.xml"* ]]; then
+  if [[ "$line" == *"src/"* ]] && [[ "$line" != *"src/package.xml"* ]]
+  then
     directory_name=$(echo "${line}" | cut -d'/' -f2)
     meta_file_exist=$(echo ${dir_meta_file_exist_array[$directory_name]})
     install -Dv $3"/${line}" $WORKING_DIR/"${line}" || { echo "Error copying file $line to $WORKING_DIR"; exit 1; }
@@ -76,16 +86,23 @@ previous_file=""
 # Skipping NON-SFDC and package.xml changes and appending relevant changes to package.xml
 for line in "${lines[@]}"; do
   echo "Processing file(s) "$iterator" out of " ${#lines[@]}
-  if [[ "$line" == *"src/"* ]] && [[ "$line" != *"src/package.xml"* ]]; then
+  if [[ "$line" == *"src/"* ]] && [[ "$line" != *"src/package.xml"* ]]
+  then
     directory_name=$(echo "${line}" | cut -d'/' -f2)
-    if [[ "${exceptional_metadata[*]}" =~ "$directory_name" ]]; then
+    if [[ "${exceptional_metadata[*]}" =~ "$directory_name" ]]
+    then
       temp_line=$(echo "${line}" | cut -d'/' -f3)
       file_name=$(echo ${temp_line//$(echo "${exceptional_metadata_suffix[$directory_name]}")})
     else
       file_name=$(echo "${line}" | cut -d'/' -f3 | cut -d'.' -f1)
     fi
     metadata_name=$(echo ${dir_xml_name_array[$directory_name]})
-    if [[ "$previous_file" != "$file_name" ]]; then
+    if [[ "$directory_name" == "classes" ]]
+    then
+      apex_classes_list+=($file_name)
+    fi
+    if [[ "$previous_file" != "$file_name" ]]
+    then
       echo "  <types>" >> "${OUTPUT_XML_FILE}"
       echo "    <members>${file_name}</members>" >> "${OUTPUT_XML_FILE}"
       echo "    <name>${metadata_name}</name>" >> "${OUTPUT_XML_FILE}"
@@ -102,4 +119,19 @@ echo "</Package>" >> "${OUTPUT_XML_FILE}"
 rm "$CHANGED_SRC_LIST_FILE" || { echo "Error: Failed to remove file $CHANGED_SRC_LIST_FILE"; exit 1; }
 mv "${OUTPUT_XML_FILE}" "$WORKING_DIR/src/package.xml" || { echo "Error: Failed to move package.xml file"; exit 1; }
 
+testClassNameList=""
+echo "Apex Class Changes Detected!!!"
+
+for class_name in "${!test_class_mapping_array[@]}"
+do
+  if [[ "${apex_classes_list[*]}" =~ "$class_name" ]]
+  then
+    test_class_name=$(echo ${test_class_mapping_array[$class_name]})
+    testClassNameList+="<runTest>${test_class_name}<\/runTest>"
+   fi
+done
+
+sed 's/<runTest><\/runTest>/'${testClassNameList}'/g' build_template.xml > build.xml
+
+unset $apex_classes_list
 echo "Processing Completed!!"
