32eadcab98a360572c1dd2822094da57ba441fbc
Akshay Gupta <26359906+akshaygupta-dev@users.noreply.github.com>
2023-06-27 14:24:54 +0000

    checking js file

--- bash-scripts/RunJSONSyntaxValidation.sh
+++ bash-scripts/RunJSONSyntaxValidation.sh
@@ -2,11 +2,15 @@
 
 jsonlint_output_log_file="jsonlint_output.log"
 
-for file in ${REPO_NAME}/changed-sources/force-app/main/default/staticresources/*.json
-do
-    echo "Checking ${file}"
-    npx jsonlint $file > ${jsonlint_output_log_file}
-done
+if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default/staticresources ]]
+then
+    echo "Change(s) detected in JSON file(s) in static resource"
+    for file in ${REPO_NAME}/changed-sources/force-app/main/default/staticresources/*.json
+    do
+        echo "Checking ${file}"
+        npx jsonlint $file > ${jsonlint_output_log_file}
+    done
+fi
 
 for line in "${jsonlint_output_log_file[@]}"; do
     if [[ "$line" == *"error"* ]] || [[ "$line" == *"Error"* ]]
