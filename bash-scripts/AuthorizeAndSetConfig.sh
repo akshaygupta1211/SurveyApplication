d99193026168d0a534e0bf2c1c9f29e890412517
Akshay Gupta <26359906+akshaygupta-dev@users.noreply.github.com>
2023-06-27 21:16:39 +0530

    fixed-validation (#124)
    * fixed validation
    
    * fixed error in json validation

--- bash-scripts/AuthorizeAndSetConfig.sh
+++ bash-scripts/AuthorizeAndSetConfig.sh
@@ -4,7 +4,7 @@
 echo "${JWT_KEY}" > jwt.key
 
 #Authorize with the org
-sfdx auth:jwt:grant --clientid ${CLIENT_KEY} --jwtkeyfile jwt.key --username ${USER_NAME}
+sfdx auth:jwt:grant --client-id ${CLIENT_KEY} --jwt-key-file jwt.key --username ${USER_NAME}
 
 #Set configuration
 sfdx config:set defaultusername=${USER_NAME}
--- bash-scripts/RunJSONSyntaxValidation.sh
+++ bash-scripts/RunJSONSyntaxValidation.sh
@@ -4,8 +4,12 @@ jsonlint_output_log_file="jsonlint_output.log"
 
 if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default/staticresources ]]
 then
-    echo "Change(s) detected in JSON file(s) in static resource"
-    for file in ${REPO_NAME}/changed-sources/force-app/main/default/staticresources/*.json
+    json_files=("$WORKING_DIR/src/objects"/*.json)
+    if [[ -v "$json_files" ]]
+    then
+        echo "Change(s) detected in JSON file(s) in static resource"
+    fi
+    for file in "${json_files[@]}"
     do
         echo "Checking ${file}"
         npx jsonlint $file > ${jsonlint_output_log_file}
