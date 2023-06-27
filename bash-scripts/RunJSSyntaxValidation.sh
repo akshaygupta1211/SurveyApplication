3bc5e1c04bfd7c0c1a5a85fc9d8275632410de1e
Akshay Gupta <26359906+akshaygupta-dev@users.noreply.github.com>
2023-06-27 15:42:13 +0000

    fixed error in json validation

--- bash-scripts/RunJSSyntaxValidation.sh
+++ bash-scripts/RunJSSyntaxValidation.sh
@@ -4,33 +4,48 @@ eslint_output_log_file="eslint_output.log"
 
 if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default/staticresources ]]
 then
-    echo "Change(s) detected in Javascript file(s) in static resource"
+    js_files=("$WORKING_DIR/src/objects"/*.js)
+    if [[ -v "$js_files" ]]
+    then
+        echo "Change(s) detected in JSON file(s) in static resource"
+    fi
     for file in ${REPO_NAME}/changed-sources/force-app/main/default/staticresources/*.js
     do
         echo "Checking ${file}"
-        npx eslint $file > ${eslint_output_log_file}
+        npx eslint $file >> ${eslint_output_log_file}
     done    
 fi
 
 if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default/aura ]]
 then
+    js_files=("$WORKING_DIR/src/objects"/*.js)
+    if [[ -v "$js_files" ]]
+    then
+        echo "Change(s) detected in Javascript file(s) in aura"
+    fi
     echo "Change(s) detected in Javascript file(s) in aura"
     for file in ${REPO_NAME}/changed-sources/force-app/main/default/aura/*.js
     do
         echo "Checking ${file}"
-        npx eslint $file > ${eslint_output_log_file}
+        npx eslint $file >> ${eslint_output_log_file}
     done    
 fi
 
 if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default/lwc ]]
 then
-    echo "Change(s) detected in Javascript file(s) in aura"
+    js_files=("$WORKING_DIR/src/objects"/*.js)
+    if [[ -v "$js_files" ]]
+    then
+        echo "Change(s) detected in Javascript file(s) in lwc"
+    fi
     for file in ${REPO_NAME}/changed-sources/force-app/main/default/lwc/*.js
     do
         echo "Checking ${file}"
-        npx eslint $file > ${eslint_output_log_file}
+        npx eslint $file >> ${eslint_output_log_file}
     done    
 fi
+
+cat $eslint_output_log_file
 
 for line in "${eslint_output_log_file[@]}"; do
     if [[ "$line" == *"error"* ]] || [[ "$line" == *"Error"* ]]
