1b13e61b8606c448d37664f5bbf5ecaed3a18245
Akshay Gupta <26359906+akshaygupta-dev@users.noreply.github.com>
2023-07-17 00:20:49 +0530

    test json validation (#130)
    * test json validation
    
    * fixed json syntax
    
    * added null check
    
    * improved security of soql
    
    * improved scripts
    
    * fixed scripts issues
    
    * fixed syntax error
    
    * added more details

--- bash-scripts/RunJSSyntaxValidation.sh
+++ bash-scripts/RunJSSyntaxValidation.sh
@@ -6,7 +6,7 @@ eslint_output_log_file="eslint_output.log"
 
 if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default/staticresources ]]
 then
-    js_files=($(find ${REPO_NAME}/changed-sources/force-app/main/default/staticresources -type f -name "*.js"))
+    js_files=($(find "${REPO_NAME}/changed-sources/force-app/main/default/staticresources" -type f -name "*.js"))
     if [[ ${#js_files[@]} -ne 0 ]]
     then
         echo "Change(s) detected in JSON file(s) in static resource"
@@ -14,13 +14,13 @@ then
     for file in "${js_files[@]}"
     do
         echo "Checking ${file}"
-        npx eslint $file >> ${eslint_output_log_file}
+        npx eslint "${file}" >> ${eslint_output_log_file}
     done    
 fi
 
 if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default/aura ]]
 then
-    js_files=($(find ${REPO_NAME}/changed-sources/force-app/main/default/aura -type f -name "*.js"))
+    js_files=($(find "${REPO_NAME}/changed-sources/force-app/main/default/aura" -type f -name "*.js"))
     if [[ ${#js_files[@]} -ne 0 ]]
     then
         echo "Change(s) detected in Javascript file(s) in aura"
@@ -29,13 +29,13 @@ then
     for file in "${js_files[@]}"
     do
         echo "Checking ${file}"
-        npx eslint $file >> ${eslint_output_log_file}
+        npx eslint "${file}" >> ${eslint_output_log_file}
     done    
 fi
 
 if [[ -d ${REPO_NAME}/changed-sources/force-app/main/default/lwc ]]
 then
-    js_files=($(find ${REPO_NAME}/changed-sources/force-app/main/default/lwc -type f -name "*.js"))
+    js_files=($(find "${REPO_NAME}/changed-sources/force-app/main/default/lwc" -type f -name "*.js"))
     if [[ ${#js_files[@]} -ne 0 ]]
     then
         echo "Change(s) detected in Javascript file(s) in lwc"
@@ -43,17 +43,14 @@ then
     for file in "${js_files[@]}"
     do
         echo "Checking ${file}"
-        npx eslint $file >> ${eslint_output_log_file}
+        npx eslint "${file}" >> ${eslint_output_log_file}
     done    
 fi
-if [ -s $eslint_output_log_file ]
+
+if [[ -s $eslint_output_log_file ]] && grep -qE 'error|Error' "$eslint_output_log_file"
 then
     cat $eslint_output_log_file
+    exit 1
+else
+    exit 0    
 fi
-
-for line in "${eslint_output_log_file[@]}"; do
-    if [[ "$line" == *"error"* ]] || [[ "$line" == *"Error"* ]]
-    then
-        exit 1
-    fi
-done
